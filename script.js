55f2b414618b4223580ad620463d0c01dcea473b
Agustin Rojas <agus.rojasmolina@gmail.com>
2022-07-04 17:19:20 -0400

    agregado un switch

--- script.js
+++ script.js
@@ -7,30 +7,75 @@
 
 alert("Se privatizó el centro pokemon, ahora curar a tus pokemón tiene un costo!") // Actualmente el valor está a 2 dólares el HP, más un costo base de 10 dólares por pokemón.
 
-numero = prompt("Ingrese cuántos pokemones tienes")
-alert("Ingrese cuánto HP le falta a cada pokemón")
-hp1 = prompt("A tu pikachu le faltan:")
-hp2 = prompt("a tu eevee le faltan:")
-hp3 = prompt("a tu charmander le faltan:")
-hp4 = prompt("a tu lugia le falta:")
-hp5 = prompt("a tu ditto le falta:")
-hp6 = prompt("a tu magikarp le falta:")
+const numero = parseFloat(prompt("Ingrese cuántos pokemones tienes"))
+alert("Ahora ingrese cuánto HP le falta a cada pokemón")
 
-function calculadorPoke (pokeNumber = 1, dolar = 10) { // funcion para calcular costo base 
-    return pokeNumber * dolar
+let hp1
+let hp2
+let hp3
+let hp4
+let hp5
+let hp6
+
+switch(numero) {
+    case 6:
+        hp1 = prompt("A tu pikachu le falta:")
+        hp2 = prompt("a tu eevee le falta:")
+        hp3 = prompt("a tu charmander le falta:")
+        hp4 = prompt("a tu lugia le falta:")
+        hp5 = prompt("a tu ditto le falta:")
+        hp6 = prompt("a tu magikarp le falta:")
+        break
+    case 5:
+        hp1 = prompt("A tu pikachu le falta:")
+        hp2 = prompt("a tu eevee le falta:")
+        hp3 = prompt("a tu charmander le falta:")
+        hp4 = prompt("a tu lugia le falta:")
+        hp5 = prompt("a tu ditto le falta:")
+        break
+    case 4:
+        hp1 = prompt("A tu pikachu le falta:")
+        hp2 = prompt("a tu eevee le falta:")
+        hp3 = prompt("a tu charmander le falta:")
+        hp4 = prompt("a tu lugia le falta:")
+        break
+    case 3:
+        hp1 = prompt("A tu pikachu le falta:")
+        hp2 = prompt("a tu eevee le falta:")
+        hp3 = prompt("a tu charmander le falta:")
+        break
+    case 2:
+        hp1 = prompt("A tu pikachu le falta:")
+        hp2 = prompt("a tu eevee le falta:")
+        break
+    case 1:
+        hp1 = prompt("A tu pikachu le falta:")
+        break
+    default:
+        alert("Numero no válido, sólo coloca el numero")
 }
-let  costoBase = calculadorPoke(numero)
 
-function calculadorHp(hp1 = 0, hp2 = 0, hp3 = 0, hp4 = 0, hp5 = 0, hp6 = 0) { // funcion para calcular costo total por hp
-    return 2 * hp1 + 2 * hp2 + 2 * hp3 + 2 * hp4 + 2 * hp4 + 2 * hp5 + 2 * hp6
+if (isNaN(numero)) {
+    alert("Inténtalo denuevo")
+} else {
+    function calculadorPoke (pokeNumber = 1, dolar = 10) { // funcion para calcular costo base 
+        return pokeNumber * dolar
+    }
+    let  costoBase = calculadorPoke(numero)
+    
+    function calculadorHp(hp1 = 0, hp2 = 0, hp3 = 0, hp4 = 0, hp5 = 0, hp6 = 0) { // funcion para calcular costo total por hp
+        return 2 * hp1 + 2 * hp2 + 2 * hp3 + 2 * hp4 + 2 * hp4 + 2 * hp5 + 2 * hp6
+    }
+    const costoHp = calculadorHp(hp1, hp2, hp3, hp4, hp5, hp6)
+    
+    
+    function sumarCosto(pokeNumber = 1, hpNumber = 0)
+    { // funcion para sumar ambos costos.
+        const costoCura = costoBase + costoHp
+        return costoCura
+    }
+    let costoTotal = sumarCosto(costoBase, costoHp)
+    
+    alert(`Curar a tus pokemon cuesta ${costoTotal} dolares!`)
 }
-const costoHp = calculadorHp(hp1, hp2, hp3, hp4, hp5, hp6)
 
-
-function sumarCosto(pokeNumber = 1, hpNumber = 0){ // funcion para sumar ambos costos.
-    const costoCura = costoBase + costoHp
-    return costoCura
-}
-let costoTotal = sumarCosto(costoBase, costoHp)
-
-alert(`Curar a tus pokemon cuesta ${costoTotal} dolares!`)
